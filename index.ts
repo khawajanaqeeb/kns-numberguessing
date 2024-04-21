32deb2696a6a9ece41b37f7c25fd4bfc03ed7dd7
Khawaja Naqeeb Uddin <161006096+khawajanaqeeb@users.noreply.github.com>
2024-04-22 04:07:23 +0500

    made number guessing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber=Math.floor(Math.random()*10);
+
+
+
+const answer=await inquirer.prompt([ 
+    {
+        name:"userGuessedNumber",
+        type:"number",
+        message:"Please guess a number between 0-10 :",
+    }
+]);
+if (answer.userGuessedNumber===randomNumber)
+    { console.log("congradulations ! your guessed right number.");}
+else {console.log("You guessed wrong number");
+}
